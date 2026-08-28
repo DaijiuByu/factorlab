3ee528022aeab7e17af0b4679f2d6910f5a9be3b
DaijiuByu <1363582823@qq.com>
2026-08-28 17:27:26 +0800

    Sync src/factorlab/__init__.py

--- src/factorlab/__init__.py
+++ src/factorlab/__init__.py
@@ -3,6 +3,13 @@
 __version__ = "0.1.0"
 
 from .data import generate_demo_panel, load_panel
+from .metrics import compute_asset_metrics
 from .research import ResearchResult, run_research
 
-__all__ = ["ResearchResult", "generate_demo_panel", "load_panel", "run_research"]
+__all__ = [
+    "ResearchResult",
+    "compute_asset_metrics",
+    "generate_demo_panel",
+    "load_panel",
+    "run_research",
+]
